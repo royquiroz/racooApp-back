4c04a4c914179067d5c0a610e000d446b64b7abf
Gonzalo Quiroz <kiyegoo@gmail.com>
2019-03-26 21:41:37 -0600

    Se agrega nueva version del front

---
+++ public/precache-manifest.2304fc826168c35807afa5cef0cdcb7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "56ebc9da6820015aeccd45ffb58bbd14",
+    "url": "/static/media/logo_min.56ebc9da.png"
+  },
+  {
+    "revision": "76c735f55895daf69cb2b59712fb7928",
+    "url": "/static/media/logo.76c735f5.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "263198938dbff4a3668e",
+    "url": "/static/js/main.26319893.chunk.js"
+  },
+  {
+    "revision": "9701eb10cfaa7d0e155e",
+    "url": "/static/js/1.9701eb10.chunk.js"
+  },
+  {
+    "revision": "263198938dbff4a3668e",
+    "url": "/static/css/main.f5bfefce.chunk.css"
+  },
+  {
+    "revision": "5bd1dc0a98747dd6dcca3f3960fc2343",
+    "url": "/index.html"
+  }
+];
